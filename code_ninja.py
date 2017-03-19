97ef7c4caa93a4267330520902399803b9da4539
Lander Bodyn <bodyn.lander@gmail.com>
2017-03-19 20:21:59 +0100

    add train mode

--- code_ninja.py
+++ code_ninja.py
@@ -5,39 +5,94 @@ import pickle
 import time
 import random
 
-class dojo_training:
+class ninja_training:
 
     def __init__(self, dojo_name):
-        self.dojo_name = dojo_name.lower()
-        self.scores_dir = 'scores/' + self.dojo_name + '.pkl'
 
-        # Get shortcuts
-        self.commands = []
-        self.meanings = []
-        with open('shortcuts/' + self.dojo_name + '.txt') as f:
+        self.time_per_answer = 15
+        self.dojo_name = dojo_name
+
+        # Directory name from where to load/save the scores
+        self.scores_dir = 'scores/' + dojo_name + '.pkl'
+
+        # Get shortcut commands and meanings
+        self.commands = {}
+        with open('shortcuts/' + dojo_name + '.txt') as f:
             for line in f:
-                command, meaning = line.split('  ')
-                self.commands.append(command)
-                self.meanings.append(meaning)
+                command, meaning = line.rstrip().split('  ')
+                self.commands[command] = meaning
 
-        # Get past scores in the commands
+        # Get dictionary with the scores for each command. Create a new empty
+        # scores dict if no scores list is found. The value of each score is a
+        # list containing: the total number of correct answers, total time to
+        # answer, number of times asked
         try:
-            with open(self.scores_dir) as f:
-                self.scores_list = pickle.load(f)
+            with open(self.scores_dir, 'rb') as f:
+                self.scores_dict = pickle.load(f)
         except FileNotFoundError:
-            scores_list = [0 for _ in range(len(self.commands))]
+            self.scores_dict = {command:[0,0,0] for command in self.commands}
+        self.save_scores()
 
-        self.scores = dict()
+    def save_scores(self):
+        with open(self.scores_dir, 'wb') as f:
+            pickle.dump(self.scores_dict, f)
 
-    def update_scores():
-        with open(self.scores_dir) as f:
-            pass
+    @staticmethod
+    def clean_terminal(number_of_lines):
+        for i in range(number_of_lines):
+            print('\033[F' + ' '*79 + '\r', end='')
 
-    def test_shortcut():
-        pass
+    def test_command(self, command):
+
+        start_time = time.time()
+        meaning = self.commands[command]
+
+        # Ask command for operation
+        answered_command = input('{}: '.format(meaning))
+
+        # Evaluate answer
+        if answered_command == 'quit':
+            quit()
+        is_correct = (command == answered_command)
+        answer_time = time.time() - start_time
+        if not is_correct:
+            qualifier = 'Wrong!'
+        elif answer_time > self.time_per_answer:
+            qualifier = 'Too slow!'
+        else:
+            qualifier = 'Correct!'
+
+        # Update and save the score statistics for the command
+        score = self.scores_dict[command]
+        score[0] += int(is_correct) 
+        score[1] += min(answer_time, self.time_per_answer)
+        score[2] += 1
+        self.save_scores()
+
+        # Give output on the answered command
+        self.clean_terminal(2)
+        output = meaning + '. Answer: ' +  answered_command
+        output += '. True: ' + command
+        output = '{:<70}'.format(output)
+        print(output + '{:>9}'.format(qualifier))
+
+    def __str__(self):
+        scores = self.scores_dict.values()
+        correct_pct = sum(score[0] for score in scores)/(sum(score[2] for score
+            in scores) + 0.000001)*100
+        return ('\nEntering the dojo of {}.\n'
+        'You have a white belt for this dojo!\n'
+        'Percentage of correct commands: {:2.2f}%'.format(self.dojo_name,
+            correct_pct))
 
     def train(self):
-        pass
+        print(self)
+        input('Press enter to start training')
+        print('\n' + '-'*79)
+        answer = None
+        while answer != 'quit':
+            command = random.choice(list(self.commands))
+            self.test_command(command)
 
     def rewards():
         pass
@@ -45,6 +100,6 @@ class dojo_training:
 if __name__ == '__main__':
 
     print('Welcome to Code Ninja!')
-    dojo_name = input('In which dojo would you like to train? ')
-    dojo = dojo_training(dojo_name)
+    dojo_name = input('In which dojo would you like to train? ').lower()
+    dojo = ninja_training(dojo_name)
     dojo.train()
